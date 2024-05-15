09ba7ce81742cb6e71b6dc8e1cfe4cbe6673fd38
DucTran04 <ductran04.hh@gmail.com>
2024-05-15 16:54:51 +0700

    Baithuchanhc4

--- Baithuchanhc4/Slide21.java
+++ Baithuchanhc4/Slide21.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class Arrlist {
+public class Slide21 {
     public static void main(String[] args) {
         ArrayList <String> colors = new ArrayList <String>();
         colors.add("red");
--- Baithuchanhc4/Slide23.java
+++ Baithuchanhc4/Slide23.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class max {
+public class Slide23 {
     public static void main(String[] args) {
         ArrayList <Integer> ArrlistInteger = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
